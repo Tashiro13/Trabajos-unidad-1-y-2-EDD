c1cb90a778ef4264fab2620db467125ea7dbeeaf
Tashiro13 <143568865+Tashiro13@users.noreply.github.com>
2024-09-28 07:24:02 -0600

    Add files via upload

---
+++ Hola_Mundo/src/hola_mundo/Hola_Mundo.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package hola_mundo;
+
+/**
+ *Numero de control: 23550373
+ * @author alex_
+ * Materia: Estructura de datos 
+ */
+public class Hola_Mundo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hola mundo!");
+    }
+    
+}
